0dea06983e7cb72918cbb43aa3e475f9660c6d8d
carlosmoreno4470777 <carlosmoreno.4470777@gmail.com>
2024-02-28 22:21:35 -0600

    all code commit

--- mysql-java/src/main/java/projects/service/ProjectService.java
+++ mysql-java/src/main/java/projects/service/ProjectService.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.NoSuchElementException;
 import projects.dao.ProjectDao;
 import projects.entity.Project;
+import projects.exception.DbException;
 
 /**
  * This class implements the service layer in the 3-tier application. The CRUD operations that the
@@ -50,6 +51,22 @@ public class ProjectService {
   public Project fetchProjectById(Integer projectId) {
     return projectDao.fetchProjectById(projectId).orElseThrow(() -> new NoSuchElementException(
         "Project with project ID=" + projectId + " does not exist."));
+  }
+
+  public void modifyProjectDetails (Project project) {
+
+		if(!projectDao.modifyProjectDetails (project)) {
+			throw new DbException("Project with ID=" + project.getProjectId() + " does not exist.");
+		}
+	}
+
+  /**
+  *	@param projectId
+  */
+  public void deleteProject (Integer projectId) {
+  	if(!projectDao.deleteProject(projectId)) {
+  		throw new DbException("Project with ID=" + projectId + " does not exist.");
+  	}
   }
 
 
